8b7794fb6a2158ebd9b5c36556b0e4d287dc1fa5
jamiemccarron <jamiemccarron2807@gmail.com>
2024-06-24 13:19:09 +0100

    calulater
    part of the calulater program

---
+++ accessing_elements/class.py
@@ -0,0 +1,26 @@
+
+class Math ():
+
+    @staticmethod
+    def add(x, y):
+        return x + y
+
+    @staticmethod
+    def sub(x,y):
+        return x-y
+
+    @staticmethod
+    def div(x,y):
+        return x/y
+
+    @staticmethod
+    def mult(x,y):
+        return x*y
+
+    @staticmethod
+    def square(x):
+        return x*x
+
+    @staticmethod
+    def cubbed(x):
+        return x*x*x
