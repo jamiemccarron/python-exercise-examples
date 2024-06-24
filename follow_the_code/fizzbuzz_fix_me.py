b6657c64130865c631c9197dcd64ed674d4b7a64
jamiemccarron <jamiemccarron2807@gmail.com>
2024-06-24 09:36:19 +0100

    Update fizzbuzz_fix_me.py
    added line breaks as example

--- follow_the_code/fizzbuzz_fix_me.py
+++ follow_the_code/fizzbuzz_fix_me.py
@@ -16,6 +16,15 @@ for x in range(0, upToo + 1):
 # Nothing below this comment is broken.
 expectedOutput = [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz!', 16, 17, 'fizz', 19, 'buzz']
 
+
+
+
+
+
+
+
+
+
 print ("The output should look like the following:")
 print (expectedOutput)
 print ("")
