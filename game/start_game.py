20f2ba520807aeaddf43a1d7e9da8fedf3914731
jamiemccarron <jamiemccarron2807@gmail.com>
2024-06-25 13:03:08 +0100

    based game code
    the code of the game before phipp code

---
+++ game/start_game.py
@@ -0,0 +1,117 @@
+def invalid ():
+    return "you ave entered an invailid letter or number"
+
+#start of code stealing the car
+print("you are a travller walking through a misstrious town that you have enver been in before.")
+print("while walking through the town you see a car that is unlocked with the door left open")
+print("you think to your self do you")
+print("(1)steal the car")
+print("(2) stop anyone else from stealing it by waiting for the owner to get back")
+print("(3) walk on and forget about the car")
+while True:
+    choice1=int(input("entre the number of the option that you want to do"))
+    if choice1==1:
+        print("you sprint towards the car and drive off")
+        print("while driving off the police see you and start to case you")
+        print("do you ")
+        print("(1) pull over")
+        print("(2) drive faster away")
+        while True:
+            choice2=int(input("enter the number of the option that you want to do"))
+            if choice2==1:
+                
+                print("you pull over for the police")
+                print("the police ask you to get out of the car and arrset you for stealing a car")
+                print("you are sent to jail for 5 years")
+                break
+            elif choice2==2:
+                
+                print("you drive faster and faster until you see a poloice barracade")
+                print("do you")
+                print("(1) drive through the barracade")
+                print("(2) stop and get arrseted by the police")
+                print("(3) get out and run away from the police")
+                while True:
+                    choice3=int(input("enter the number of the option that you want to do"))
+                    if choice3==1:
+                        print("you drive through the barracade and you get lunched into the air.")
+                        print("mid air you car cacths fire and hit the ground and exploeds")
+                        print("you die")
+                        break
+                    elif choice3==2:
+                        print("you pull over for the police")
+                        print("the police ask you to get out of the car and arrset you for stealing a car")
+                        print("you are sent to jail for 5 years")
+                        break
+                    elif choice3==3:
+                        print("you get out of the car and start to run")
+                        print("the police right behide are yelling at you to stop ruuning")
+                        print("do you")
+                        print("(1) contine to run away")
+                        print("(2) stop and allow the police to arrest you")
+                        while True:
+                            choice4=int(input("enter the number of the option you want to do"))
+                            if choice4==1:
+                                print("you contiune to run away but all of a sudden you have this electric feeling in your in your back ")
+                                print("you then relise that you are unable to move and are about to collabes")
+                                print("you then relise that you have been tanzered.")
+                                print("you have been arrsed and sent to jail for 10 years for stealing a car speeding and running away from police.")
+                                
+                            elif choice4==2:
+                                print("you stop for police and they tackle you to the ground and then and cuff you")
+                                print("you are arresd and sent to jail for 10 years for speeding stealing a car and running away from police.")
+                            else:
+                                print(invalid())  
+                    else:
+                        print(invalid())
+        else:
+            print(invalid())
+            
+    elif choice1==2:
+        print("you stand by the car waiting for the owner to get back")
+        print("you wait for about 10 minutes until the owner finaly come back")
+        print("the owner offers to pay you for protecting his car")
+        print("but he only has a £50 note")
+        print("what do you do")
+        print("(1) take the the £50")
+        print("(2) thank the owner for the offer and walk away")
+        while True:
+            if choice5==1:
+                print("you take the £50 and walk away")
+                break
+            elif choice5==2:
+                print("you leave the £50 and walk away")
+                break
+            else:
+                print("you have entred an invaild letter or number")
+                
+    elif choice1==3:
+        break
+        
+    else:
+        print(invalid())
+
+print("walking down the street you see the car you just left speed down the road beside you")
+print("you watch the car take a conoer fast and as soon as it get out of your vistion you hear a massive bang")
+print("you realise that the car has crash and go to to make sure that everyone is ok ")
+print("when you get there you see the care upside down and someone is yelling form inside the car")
+print("you are able to tell that the care has crashed becasue it rolled up the curb")
+print("do you help the person out of the car")
+print("(1) yes")
+print("(2) no")
+while True:
+    choice6=int(input("enter the number of the option you want to chouse"))
+    if choice6==1:
+        print("as you bend down to help the man in the car the car sunddenly explodeds with fire")
+        print("you know you dont have long to save the man you quickly get in the car and help the man out of the car")
+        print("as the man gets out of the car he ask for you to bring him to hosipaial")
+        print("what do you do")
+        print("say yes")
+        print("say no")
+        print("say that you don't have a car")
+        while
+    elif choice6==2:
+    else:
+        print(invalid())
+    
+
